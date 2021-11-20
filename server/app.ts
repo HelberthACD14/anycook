c2a7c626c5b2e4fe15d6dce8c52a7429f8b1591e
HelberthACD14 <helberth1488@gmail.com>
2021-11-20 17:53:48 -0500

    Ajustes iniciales aplicacion node-Vue

---
+++ server/app.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import path from "path";
+
+import tasksRoutes from "./routes";
+
+const app = express();
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.use("/api", tasksRoutes);
+
+// app.use(express.static(path.join(__dirname, "..", "..", "dist")));
+//cd Server
+//npm run build
+//npm start
+//navigate to localhost 3000
+
+
+export default app;
